1f87361a3c0ec22d7af61feae2c7ef11c0c4dbda
s3850825 <s3850825@student.rmit.edu>
2022-03-30 00:13:18 +1100

    aws cognito config

--- FrontEnd/cloudcomputingapp/src/configs/aws.ts
+++
@@ -1,5 +0,0 @@
-export const COGNITO = {
-    REGION: "",
-    USER_POOL_ID: "",
-    APP_CLIENT_ID: "",
-};
---
+++ FrontEnd/cloudcomputingapp/src/configs/awsconfig.js
@@ -0,0 +1,7 @@
+export default {
+    Auth: {
+        region: "us-east-1",
+        userPoolId: "us-east-1_PRNGAeFOF",
+        userPoolWebClientId: "q0o9bg7bpkjngbvo7m7opsvmt"
+    }
+}
